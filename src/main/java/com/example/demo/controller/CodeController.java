10e1b44249ec1645f1383f42635fa85e093338f1
en91neer <en91neer@gmail.com>
2026-06-25 22:57:39 +0900

    오늘테스트끝

--- src/main/java/com/example/demo/controller/CodeController.java
+++ src/main/java/com/example/demo/controller/CodeController.java
@@ -87,7 +87,7 @@ public class CodeController {
         authService.validateAdmin(loginEmail, authToken);
         codeService.deleteGroup(id);
 
-        return "삭제 완료11";
+        return "삭제 완료";
     }
 
     @DeleteMapping("/items/{id}")
